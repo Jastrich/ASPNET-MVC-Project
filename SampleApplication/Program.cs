ffe0a517812808dc5393ceb8bd77a014a8e0d389
Jacob Stricherz <jacob.stricherz@mines.sdsmt.edu>
2023-02-25 14:04:07 -0600

    Added Entity Framework Core

--- SampleApplication/Program.cs
+++ SampleApplication/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using SampleApplication.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,10 @@ builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+});
 
 var app = builder.Build();
 
